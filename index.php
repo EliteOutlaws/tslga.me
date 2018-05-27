7a6c5523b9d3d6028b68477d899bf847da23c32d
ismailimanov <ismail@imanov.dk>
2018-05-27 21:45:09 +0200

    Design

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
+<?php
+// include("inc/includes.php")
+?>
 <!doctype html>
 <html lang="en">
     <head>
         <title>TSLGame Database - Find the newest tslgame.exe files here!</title>
         <meta charset="utf-8">
         <link href="css/main.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     </head>
     <body>
-        <?php
-            include("inc/includes.php")
-        ?>
-        Test
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="box">
+                        <h1>Files</h1>
+                        <div class="divider"></div>
+                        <div class="files">
+                            <div class="fileName">asd</div>
+                            <div class="fileDate">asd</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="box">
+                        <h1>Info</h1>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <span class="copy">Made with <i class="fa fa-heart"></i> by <a href="https://github.com/ismailimanov/" target="_blank">Imanov</a></span>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
